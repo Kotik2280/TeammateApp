f88ba8adec0d784f6a1742808cb5e619536d513a
Артём Назаров <brokeprotocol4916@gmail.com>
2025-04-30 14:50:32 +0500

    Adding validation

--- TeammateApp/Models/Post.cs
+++ TeammateApp/Models/Post.cs
@@ -1,9 +1,15 @@
-﻿namespace TeammateApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeammateApp.Models
 {
     public class Post
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Необходимо указать заголовок поста")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Заголовок не может быть короче 3 символов и длиннее 30")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Необходимо указать описание поста")]
+        [StringLength(300, MinimumLength = 10, ErrorMessage = "Описание не может быть короче 10 символов и длиннее 300")]
         public string Description { get; set; }
         public string Author { get; set; }
         public DateTime Date { get; set; }
--- TeammateApp/Models/User.cs
+++ TeammateApp/Models/User.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.Identity.Client;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace TeammateApp.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Необходимо указать имя")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Имя не должно быть короче 3 символов и длиннее 30")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Необходимо указать пароль")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Пароль не должен быть короче 6 символов и длиннее 50")]
         public string Password { get; set; }
     }
 }
